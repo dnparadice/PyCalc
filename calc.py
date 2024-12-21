5d4babcda0c04b899fb7deebbaef525383ae1482
Jay C <jcollins@accelrf.com>
2024-12-21 11:43:38 -0700

    - fixed a bug that rounded small values to zero when editing the value of locals.

--- calc.py
+++ calc.py
@@ -1270,7 +1270,7 @@ class Calculator:
         if isinstance(x, str):
             try:
                 val = float(x)
-                if '.' in str(val):
+                if '.' in str(val) or val < 1:
                     # this is explicitly a float, like 34.0, it can be cast to an int but the user has added the .0
                     return val # -------------------------------------------------------------------------------------->
                 else:
